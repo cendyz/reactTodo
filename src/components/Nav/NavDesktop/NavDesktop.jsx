6cfa898244a0291a7ae918df6427f04821282900
cendyz <arek57823@gmail.com>
2024-12-16 20:02:53 +0100

    nav building

--- src/components/Nav/NavDesktop/NavDesktop.jsx
+++ src/components/Nav/NavDesktop/NavDesktop.jsx
@@ -1,6 +1,51 @@
 import './NavDesktop.scss'
+import { mobileMenuData, mobileInnerMenu } from '../../../data'
+import { nanoid } from 'nanoid'
+import { useState } from 'react'
 
 const NavDesktop = () => {
-	return <nav className='nav-desktop'></nav>
+	const [active, setActive] = useState(null)
+
+	const handleActive = index => {
+		setActive(item => (item === index ? null : index))
+	}
+
+	return (
+		<nav className='nav-desktop'>
+			<div className='left-box'>
+				<img src='src/images/logo.svg' alt='logo' className='nav-logo' />
+				{mobileMenuData.map(({ name, group }, index) => {
+					const actualGroup = mobileInnerMenu.find(
+						item => item.group === group
+					)
+					return (
+						<div className='link-container' key={nanoid()}>
+							<div
+								className='click-box'
+								onClick={() => handleActive(index)}>
+								<a href='#' className='desktop-main-link'>
+									{name}
+								</a>
+								<img
+									src='src/images/icon-arrow-light.svg'
+									alt='arrow icon'
+								/>
+							</div>
+							<div className='links-box' key={nanoid()}>
+								{active === index &&
+									actualGroup.links.map(link => {
+										return (
+											<a href='#' className='desktop-link'>
+												{link}
+											</a>
+										)
+									})}
+							</div>
+						</div>
+					)
+				})}
+			</div>
+		</nav>
+	)
 }
 export default NavDesktop
