5bb8c6ac37224c4e7d76106821def17e9a567387
cendyz <arek57823@gmail.com>
2024-12-17 17:52:57 +0100

    nav ok

--- src/components/Nav/NavDesktop/NavDesktop.jsx
+++ src/components/Nav/NavDesktop/NavDesktop.jsx
@@ -3,11 +3,30 @@ import { mobileMenuData, mobileInnerMenu } from '../../../data'
 import { nanoid } from 'nanoid'
 import { useState } from 'react'
 
+import arrowImg from '../../../images/icon-arrow-light.svg'
+
 const NavDesktop = () => {
 	const [active, setActive] = useState(null)
+	const [activeItems, setActiveItems] = useState(new Set())
+	const [unActvieItems, setUnActiveItems] = useState(new Set())
 
 	const handleActive = index => {
 		setActive(item => (item === index ? null : index))
+		setActiveItems(prevItem => {
+			const newItems = new Set(prevItem)
+			if (newItems.has(index)) {
+				newItems.delete(index)
+				setUnActiveItems(unPrevItem => new Set(unPrevItem).add(index))
+			} else {
+				newItems.add(index)
+				setUnActiveItems(unPrevItem => {
+					const actualItems = new Set(unPrevItem)
+					actualItems.delete(index)
+					return actualItems
+				})
+			}
+			return newItems
+		})
 	}
 
 	return (
@@ -19,7 +38,7 @@ const NavDesktop = () => {
 						item => item.group === group
 					)
 					return (
-						<div className='link-container' key={nanoid()}>
+						<div className='link-container' key={index}>
 							<div
 								className='click-box'
 								onClick={() => handleActive(index)}>
@@ -27,15 +46,22 @@ const NavDesktop = () => {
 									{name}
 								</a>
 								<img
-									src='src/images/icon-arrow-light.svg'
+									src={arrowImg}
 									alt='arrow icon'
+									className={`arrow-img ${
+										active === index
+											? 'rotated'
+											: activeItems.has(index) || unActvieItems.has(index)
+											? 'no-rotated'
+											: ''
+									}`}
 								/>
 							</div>
-							<div className='links-box' key={nanoid()}>
+							<div className='links-box'>
 								{active === index &&
 									actualGroup.links.map(link => {
 										return (
-											<a href='#' className='desktop-link'>
+											<a href='#' className='desktop-link' key={nanoid()}>
 												{link}
 											</a>
 										)
@@ -44,6 +70,10 @@ const NavDesktop = () => {
 						</div>
 					)
 				})}
+			</div>
+			<div className='right-box'>
+				<button className='login-btn'>Login</button>
+				<button className='sign-btn'>Sign Up</button>
 			</div>
 		</nav>
 	)
