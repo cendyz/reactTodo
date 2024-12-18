edd8deb319d0e68a009b23161b767750cb508749
cendyz <arek57823@gmail.com>
2024-12-18 07:13:42 +0100

    section one ok desktop

--- src/components/Main/SectionOne/SectionOne.jsx
+++ src/components/Main/SectionOne/SectionOne.jsx
@@ -2,21 +2,27 @@ import { nanoid } from 'nanoid'
 import React from 'react'
 
 import editorImg from '../../../images/mobile/illustration-editor-mobile.svg'
+import editorImgDesktop from '../../../images/desktop/illustration-editor-desktop.svg'
 import { sectionOneData } from '../../../data'
 
 const SectionOne = () => {
 	return (
 		<section className='section'>
 			<h2 className='section-title'>Designed fo the future</h2>
-			<img src={editorImg} alt='Camera picture' className='section-img' />
-			{sectionOneData.map(({ title, text }) => {
-				return (
-					<React.Fragment key={nanoid()}>
-						<h2 className='section-title'>{title}</h2>
-						<p className='section-text'>{text}</p>
-					</React.Fragment>
-				)
-			})}
+			<picture>
+			<source srcSet={editorImgDesktop} media='(min-width: 992px)'/>
+			<img src={editorImg} alt='Camera picture' className='section-img camera-img' />
+			</picture>
+			<div className='text-box'>
+				{sectionOneData.map(({ title, text }) => {
+					return (
+						<React.Fragment key={nanoid()}>
+							<h3 className='section-under-title'>{title}</h3>
+							<p className='section-text'>{text}</p>
+						</React.Fragment>
+					)
+				})}
+			</div>
 		</section>
 	)
 }
